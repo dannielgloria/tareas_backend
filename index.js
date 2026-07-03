fdce8608dbb9e9e8a86885b6fc6e36976ac7e190
Daniel Gloria Florencio <danniel.gloria@gmail.com>
2026-07-02 20:24:58 -0600

    Add CORS middleware to Express app

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require("cors");
 require('dotenv').config();
 const tareasRoutes = require('./routes/tareasRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/tareas', tareasRoutes);
 
